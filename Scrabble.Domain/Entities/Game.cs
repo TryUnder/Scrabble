c5caf97de42068f0eefbdeaa215af2ee22f157ca
TryUnder <sniacyinaczej@gmail.com>
2025-02-13 20:24:55 +0100

    Dodaj pliki projektów.

---
+++ Scrabble.Domain/Entities/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrabble.Domain.Entities
+{
+    public class Game
+    {
+        public Guid GameId { get; set; }
+        public Board Board { get; set; }
+        public Tuple<Player, Player> Players { get; set; }
+        public int currentPlayerIndex { get; set; }
+
+        public Game()
+        {
+            GameId = Guid.NewGuid();
+            Board = new Board();
+            Players = Tuple.Create<Player, Player>(null, null);
+        }
+    }
+}
---
+++ Scrabble.Domain/Entities/Tile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrabble.Domain.Entities
+{
+    public class Tile
+    {
+        public char Letter { get; }
+        public int Points { get; }
+        public Tile(char letter, int points)
+        {
+            Letter = letter;
+            Points = points;
+        }
+    }
+}
