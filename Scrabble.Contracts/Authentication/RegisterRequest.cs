6c00eeebf5e8e6f4834c513379596e402555c4d9
TryUnder <sniacyinaczej@gmail.com>
2025-03-29 18:15:22 +0100

    Adding authorization contracts

---
+++ Scrabble.Contracts/Authentication/RegisterRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrabble.Contracts.Authentication
+{
+    public record RegisterRequest(
+        string Login,
+        string Email,
+        string Password
+    );
+}
