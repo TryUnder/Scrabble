46e5a94706f145db3137065d93e4e1e12f2866bd
TryUnder <sniacyinaczej@gmail.com>
2025-03-29 18:42:02 +0100

    1. Added solution dependencies between projects 2. Basic authentication controller implementation

---
+++ WebAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Scrabble.Contracts.Authentication;
+
+namespace WebAPI.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest registerRequest)
+        {
+            return Ok(registerRequest);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest loginRequest)
+        {
+            return Ok(loginRequest);
+        }
+    }
+}
